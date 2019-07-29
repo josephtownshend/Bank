4e9f0de500efbe1d30386a660c85f080ff117143
joseph townshend <joetownshend@mac.com>
2019-07-29 12:37:37 +0100

    test: refactored user tests to have @user

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,11 @@
 require 'user'
 
 RSpec.describe User do
+  before(:each) do
+    @user = User.new
+  end
 
   it "By default Account array is empty" do
-    expect(subject.get_account).to eq []
+    expect(@user.get_account).to eq []
   end
 end
