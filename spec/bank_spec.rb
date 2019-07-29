40d0c98d01db43a577ecb4ec826f96a308b2be01
joseph townshend <joetownshend@mac.com>
2019-07-29 11:55:24 +0100

    tests: refactored tests to remove subject

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,22 +1,26 @@
 require 'bank'
 
 RSpec.describe Bank do
+  before(:each) do
+    @bank = Bank.new
+  end
+
   it "Default balance of zero" do
-    expect(subject.get_balance).to eq 0
+    expect(@bank.get_balance).to eq 0
   end
 
   describe "#deposit" do
     it "Can deposit funds into account" do
-    subject.deposit(100)
-    expect(subject.get_balance).to eq 100
+      @bank.deposit(100)
+    expect(@bank.get_balance).to eq 100
   end
 end
 
 describe "#withdaw" do
   it "Can withdaw funds from account" do
-    subject.deposit(100)
-    subject.withdraw(50)
-    expect(subject.get_balance).to eq 50
+    @bank.deposit(100)
+    @bank.withdraw(50)
+    expect(@bank.get_balance).to eq 50
   end
 end
 
