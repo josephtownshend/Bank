38bc03b7a992396982fa418b59c127dfe6d24fe6
joseph townshend <joetownshend@mac.com>
2019-07-29 12:15:20 +0100

    test: refactored statement_spec with before each

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,9 +2,14 @@ require 'statement'
 
 
 RSpec.describe Statement do
-
-it "Can return the first line of statment" do
-  expect(subject.print_first_line).to eq "date || credit || debit || balance"
-
+  before(:each) do
+    @statement = Statement.new
   end
+
+  describe "#print_first_line" do
+    it "Can return the first line of statment" do
+      expect(@statement.print_first_line).to eq "date || credit || debit || balance"
+    end
+  end
+
 end
