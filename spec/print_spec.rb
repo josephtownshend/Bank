ce15d6978fd2e8c350f3d50b43e8cfc374a5761c
joseph townshend <joetownshend@mac.com>
2019-07-31 16:41:14 +0100

    refactor: tidy up after rubocop

--- spec/print_spec.rb
+++ spec/print_spec.rb
@@ -12,11 +12,20 @@ RSpec.describe Print do
     end
   end
 
-  describe 'create_statement' do
+  describe '#create_statement' do
     it 'Creates the statement' do
-      transaction_history = ["12/11/1990 || || 500.0 || 2500.0\n12/11/1990 || 2000.0 || || 3000.0\n12/11/1990 || 1000.0 || || 1000.0"]
+      transaction_history = [
+        '12/11/1990 || 1000.0 || || 1000.0',
+        '12/11/1990 || 2000.0 || || 3000.0',
+        '12/11/1990 || || 500.0 || 2500.0'
+      ]
 
-      expect(@print.create_statement(transaction_history)).to eq "date || credit || debit || balance\n12/11/1990 || || 500.0 || 2500.0\n12/11/1990 || 2000.0 || || 3000.0\n12/11/1990 || 1000.0 || || 1000.0"
+      expect(@print.create_statement(transaction_history)).to eq [
+        'date || credit || debit || balance',
+        '12/11/1990 || || 500.0 || 2500.0',
+        '12/11/1990 || 2000.0 || || 3000.0',
+        '12/11/1990 || 1000.0 || || 1000.0'
+      ].join("\n")
     end
   end
 end
